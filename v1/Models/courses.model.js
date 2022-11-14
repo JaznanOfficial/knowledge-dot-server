970f473eb71641fdeadfb9190d33d295c293b022
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-14 20:56:27 +0600

    modify courses model

--- v1/Models/courses.model.js
+++ v1/Models/courses.model.js
@@ -3,25 +3,15 @@ var validator = require("validator");
 
 const coursesSchema = mongoose.Schema(
     {
+        uname: {
+            unique: [true, "User Name must be unique"],
+            type: String,
+            required: [true, "User Name is required"],
+        },
         name: {
-            unique:[true, "Name must be unique"],
+            unique: [true, "Name must be unique"],
             type: String,
             required: [true, "Name is required"],
-        },
-        trailer_video: {
-            type: String,
-            required: [true, "trailer_video is required"],
-            // unique: [true, "Email must be unique"],
-            validate: [validator.isURL, "Please provide an URL"],
-        },
-        thumb_img: {
-            required: true,
-            type: String,
-            validate: [validator.isURL, "Please provide an URL"],
-        },
-        instructor: {
-            required: true,
-            type: String,
         },
         level: {
             required: true,
@@ -32,22 +22,67 @@ const coursesSchema = mongoose.Schema(
                     "{VALUE} isn't valid. please select 'beginner', 'intermediate', or 'expert' ",
             },
         },
-        category_two: {
+        badge: {
             required: true,
             type: String,
             enum: {
-                values: ["normal", "popular"],
+                values: ["normal", "popular", "top"],
                 message:
-                    "{VALUE} isn't valid. please select 'normal', 'popular' ",
+                    "{VALUE} isn't valid. please select 'normal', 'popular', 'top ",
             },
         },
-        price: {
-            required: true,
+        duration: {
+            unique: [false],
             type: Number,
+            required: [true, "Duration is required"],
         },
-        description: {
+        language: {
             required: true,
             type: String,
+            enum: {
+                values: ["Bengali", "English", "Hindi"],
+                message:
+                    "{VALUE} isn't valid. please select 'Bengali', 'English', or 'Hindi' ",
+            },
+        },
+        accessibility: {
+            unique: [false],
+            type: String,
+            required: [true, "Accessibility is required"],
+        },
+        instructor: {
+            type: String,
+            required: [true, "Instructor is required"],
+        },
+        instructorimg: {
+            required: true,
+            type: String,
+            validate: [validator.isURL, "Please provide Instructor Image URL"],
+        },
+        thumbimg: {
+            required: true,
+            type: String,
+            validate: [validator.isURL, "Please provide Thumbnail Image URL"],
+        },
+        introvdo: {
+            type: String,
+            required: [true, "Introduction Video is required"],
+            // unique: [true, "Email must be unique"],
+            validate: [validator.isURL, "Please provide Introduction Video URL"],
+        },
+        list: {
+            required: true,
+            type: String,
+            enum: {
+                values: ["design", "development", "learning"],
+                message:
+                    "{VALUE} isn't valid. please select 'design', 'development', 'learning ",
+            },
+        },
+        category: {
+            type: String,
+            required: true,
+            
         },
         rating: {
             total_rating: {
@@ -57,22 +92,43 @@ const coursesSchema = mongoose.Schema(
                 type: Number,
             },
         },
-        category: {
-            type: String,
-            required:true,
-            
+        details: {
+            description: {
+                type: String,
+            },
+            certificate_details: {
+                type: String,
+                
+            },
+            for_whom: {
+                type: String
+            },
         },
-        certificate_details: {
-            type: String,
-
+        files: [{
+            title: {
+                type: String,
+                required: [true, "Title is required"],
+            },
+            details: [{
+                name: {
+                    type: String,
+                    required: [true, "Name is required"],
+                },
+                fileName: {
+                    type: String,
+                    required: [true, "File Name is required"],
+                },
+                vurl: {
+                    type: String,
+                    required: [true, "Video URL is required"],
+                    validate: [validator.isURL, "Please provide a Video URL"],
+                },
+            }],
+        }],
+        price: {
+            type: Number,
+            required: [true, "Price is required"],
         },
-        for_whom: {
-            type: String
-        },
-        video: [{
-            type: String,
-            validate: [validator.isURL, "Please provide an URL"],
-        }]
     },
     {
         timestamps: true,
